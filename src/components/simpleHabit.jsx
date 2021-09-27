7340b5e18c3d9d2d49a2bcc41a4db2e2b3be3875
jkim8 <juno0218@naver.com>
2021-09-27 16:03:28 -0400

    useCallback and useEffect

--- src/components/simpleHabit.jsx
+++ src/components/simpleHabit.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 const SimpleHabit = (props) => {
   const [count, setCount] = useState(0);
-  const handleIncrement = () => {
+  const spanRef = useRef();
+  const handleIncrement = useCallback(() => {
     setCount(count + 1);
-  };
+  });
+
+  useEffect(() => {
+    console.log("mounted and uptared");
+  }, []);
   return (
     <li className="habit">
-      <span className="habit-name">Reading</span>
+      <span ref={spanRef} className="habit-name">
+        Reading
+      </span>
       <span className="habit-count">{count}</span>
       <button className="habit-button habit-increase" onClick={handleIncrement}>
         <i className="fas fa-plus-square"></i>
