2b87b453d5a089c98594534d7beec1f7b7f4e0ad
jkim8 <juno0218@naver.com>
2021-09-27 15:52:50 -0400

    using useState

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import '@fortawesome/fontawesome-free/js/all.js';
+import SimpleHabit from './components/simpleHabit';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <SimpleHabit />
   </React.StrictMode>,
   document.getElementById('root')
 );
